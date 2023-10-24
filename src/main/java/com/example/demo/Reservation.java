8324fbd59e67b3af701d07b7eed28fe8495188f1
sreejithsundaram <sreejithsundaram@outlook.com>
2023-10-24 17:43:56 +0530

    added date formatting

--- src/main/java/com/example/demo/Reservation.java
+++ src/main/java/com/example/demo/Reservation.java
@@ -8,6 +8,8 @@ import java.util.*;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,7 +19,9 @@ public class Reservation {
     public String id;
     public String guestEmail;
     public String guestName;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     public Date checkInDate;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     public Date checkOutDate;
     public String roomNumber;
 }
