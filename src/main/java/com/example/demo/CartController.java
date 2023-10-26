e0f1cc7f704a330a3382505f735f8f4ddd0e5f3e
sreejithsundaram <sreejithsundaram@outlook.com>
2023-10-26 18:23:03 +0530

    added delete, post

--- src/main/java/com/example/demo/CartController.java
+++ src/main/java/com/example/demo/CartController.java
@@ -3,9 +3,13 @@ package com.example.demo;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @CrossOrigin
@@ -18,5 +22,13 @@ public class CartController {
     @GetMapping("/")
     List<cart> getAll(){
         return cartRepository.findAll();
+    }
+    @DeleteMapping("/")
+    void deleteAll(){
+        cartRepository.deleteAll(); 
+    }
+    @PostMapping("/") 
+    cart save(@RequestBody cart cart_){
+        return cartRepository.save(cart_);
     }
 }
